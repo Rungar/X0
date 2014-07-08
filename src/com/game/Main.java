635807a0328c19ddd1881118331326078f334bc4
Rungar <skpalantir@gmail.com>
2014-07-08 08:21:00 +0600

    initial project version

---
+++ src/com/game/Main.java
@@ -0,0 +1,35 @@
+package com.game;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//import com.game.SquareXO;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        //String name = reader.readLine();
+        //System.out.println(name);
+        SquareXO mySquare = new SquareXO();
+        mySquare.eraseField();
+        mySquare.showField();
+        int i = 0;
+        boolean whoIs = true;
+        char who = ' ';
+        while (!mySquare.win(who) && i < 9) {
+            if (whoIs) {
+                who = 'X';
+            } else {
+                who = 'O';
+            }
+            System.out.println("Make your move " + who);
+            if (mySquare.markField(Integer.parseInt(reader.readLine()),who)) {
+                i++;
+                whoIs = !whoIs;
+            } else {
+                System.out.println("Wrong move");
+            }
+         }
+
+    }
+}
